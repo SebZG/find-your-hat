70dc9b5d712d1b84203c5027cf5692c9d02dbc5c
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-31 13:52:44 +0100

    Add Print()

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+   constructor(field = [[]]) {
+      this.field = field;
+      this.X = 0;
+      this.Y = 0;
+      this.field[0][0] = pathCharacter;
+   }
+
+   print() {
+      this.field.forEach(row =>
+         console.log(row.join('')))
+         .join("\n");
+   }
+
+
+}
