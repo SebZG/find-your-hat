0b0e844066304a7a55fe281af6b0350a2260cfab
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-07-31 15:22:26 +0100

    Add generateField()

--- main.js
+++ main.js
@@ -14,29 +14,75 @@ class Field {
       this.field[0][0] = pathCharacter;
    }
 
+   static generateField(height, width, percentage = 0.1) {
+      const field = new Array(height).fill(0).map(el => new Array(width));
+      for (let y = 0; y < height; y++) {
+         for (let x = 0; x < width; x++) {
+            const prob = Math.random();
+            field[y][x] = prob > percentage ? fieldCharacter : hole;
+         }
+      }
+      // Set the "hat" location
+      const hatLocation = {
+         x: Math.floor(Math.random() * width),
+         y: Math.floor(Math.random() * height)
+      };
+      // Make sure the "hat" is not at the starting point
+      while (hatLocation.x === 0 && hatLocation.y === 0) {
+         hatLocation.x = Math.floor(Math.random() * width);
+         hatLocation.y = Math.floor(Math.random() * height);
+      }
+      field[hatLocation.y][hatLocation.x] = hat;
+      return field;
+   }
+
+   play() {
+      let playing = true;
+      while (playing) {
+         this.print();
+         this.ask();
+         if (!this.isInBounds()) {
+            console.log('Out of bounds!');
+            playing = false;
+            break;
+         } else if (this.isHole()) {
+            console.log('You fell down a hole!');
+            playing = false;
+            break;
+         } else if (this.isHat()) {
+            console.log('You found your hat!');
+            playing = false;
+            break;
+         }
+         // Update the current location on the map
+         this.field[this.Y][this.X] = pathCharacter;
+      }
+   }
+
    print() {
-      this.field.forEach(row =>
-         console.log(row.join('')))
-         .join("\n");
+      const displayString = this.field.map(row => {
+         return row.join('');
+      }).join('\n');
+      console.log(displayString);
    }
 
    ask() {
-      const answer = prompt("Which way? ").toUpperCase();
+      const answer = prompt('Which way? ').toUpperCase();
       switch (answer) {
-         case "U":
+         case 'U':
             this.Y -= 1;
             break;
-         case "D":
+         case 'D':
             this.Y += 1;
             break;
-         case "L":
+         case 'L':
             this.X -= 1;
             break;
-         case "R":
+         case 'R':
             this.X += 1;
             break;
          default:
-            console.log("Enter U, D, L or R");
+            console.log('Enter U, D, L or R.');
             this.ask();
             break;
       }
@@ -50,7 +96,6 @@ class Field {
          this.X < this.field[0].length
       );
    }
-
    isHole() {
       return this.field[this.Y][this.X] === hole;
    }
@@ -58,29 +103,8 @@ class Field {
    isHat() {
       return this.field[this.Y][this.X] === hat;
    }
-
-   play() {
-      let playing = true;
-      while (playing) {
-         this.print();
-         this.ask();
-         if (!this.isInBounds()) {
-            console.log("Out of bounds!");
-            playing = false;
-            break;
-         } else if (this.isHole()) {
-            console.log("You fell down a hole!");
-            playing = false;
-            break;
-         } else if (this.isHat()) {
-            console.log("You found your hat!");
-            playing = false;
-            break;
-         }
-         // Update the current location on the map
-         this.field[this.Y][this.X] = pathCharacter;
-      }
-   }
-
-
 }
+
+const myfield = new Field(Field.generateField(10, 10, 0.2));
+
+myfield.play();
